960c9d8265cd7cc296efaabf737acab5902509b6
GabrielGem <gabrielgarciamendes91@gmail.com>
2025-11-26 17:04:36 -0300

    optimazed: calc math in line and colors placed directly into memory

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 17:41:38 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/11/25 17:10:37 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2025/11/26 14:37:12 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,86 +35,59 @@ void	fractol_init(t_fractol *fractol)
 			fractol->mlx, WIDTH, HEIGHT, fractol->name_win);
 	fractol->img.layer = mlx_new_image(fractol->mlx, WIDTH, HEIGHT);
 	fractol->img.addr = mlx_get_data_addr(
-			fractol->img.layer, &fractol->img.bpp, &fractol->img.line_len,
+			fractol->img.layer,
+			&fractol->img.bpp,
+			&fractol->img.line_len,
 			&fractol->img.endian);
-}
-
-double	pantograph(double unscaled, double old_max,
-		double new_min, double new_max)
-{
-	return ((new_max - new_min) * (unscaled - 0) / (old_max - 0) + new_min);
+	fractol->coordinates.r = (2.0 - (-2.0)) / WIDTH;
+	fractol->coordinates.i = (-2.0 - 2.0) / HEIGHT;
 }
 
 void	fractol_render(t_fractol *fractol)
 {
-	int	x;
-	int	y;
+	int			x;
+	int			y;
+	int			*pixel_ptr;
+	int			out;
 
-	x = 0;
-	while (x < HEIGHT)
+	pixel_ptr = (int *)fractol->img.addr;
+	y = 0;
+	while (y < HEIGHT)
 	{
-		y = 0;
-		while (y < HEIGHT)
+		x = 0;
+		while (x < WIDTH)
 		{
-			handle_pixel(x, y, fractol);
-			y++;
+			out = handle_pixel(x, y, fractol->coordinates);
+			if (out == ITERATIONS)
+				*pixel_ptr = BLACK;
+			else
+				*pixel_ptr = (out * 2) << 16 | (out * 4) << 8 | (out * 8);
+			pixel_ptr++;
+			x++;
 		}
-		x++;
+		y++;
 	}
 	mlx_put_image_to_window(fractol->mlx, fractol->mlx_win,
-				fractol->img.layer, 0, 0);
+		fractol->img.layer, 0, 0);
 }
 
-void	handle_pixel(int x, int y, t_fractol *fractol)
+int	handle_pixel(int x, int y, t_complex coordinates)
 {
 	t_complex	z;
 	t_complex	c;
 	int			i;
-	int			iterations;
-	int			color;
 
-	z.r = 0.0;
-	z.i = 0.0;
-	c.r = pantograph(x, WIDTH, -2, +2);
-	c.i = pantograph(y, HEIGHT, +2, -2);
+	z = (t_complex){.r = 0.0, .i = 0.0};
+	c.r = -2.0 + (x * coordinates.r);
+	c.i = 2.0 + (y * coordinates.i);
 	i = 0;
-	iterations = 42;
-	while (i < iterations) // 42 number of iterarions
+	while (i < ITERATIONS)
 	{
-		z = sum_complex(distributive(z), c);
-		if ((z.r * z.r) + (z.i * z.i) > 4)
-		{
-			color = pantograph(i, iterations, BLACK, WHITE);
-			ft_mlx_pixel_put(&fractol->img, x, y, color);
-			return ;
-		}
+		if ((z.r * z.r) + (z.i * z.i) > 4.0)
+			return (i);
+		z = (t_complex){.r = (((z.r * z.r) - (z.i * z.i)) + c.r),
+			.i = ((2 * z.r * z.i) + c.i)};
 		i++;
 	}
-	ft_mlx_pixel_put(&fractol->img, x, y, LAVA);
-}
-
-void	ft_mlx_pixel_put(t_data *data, int x, int y, int color)
-{
-	int	offset;
-
-	offset = (y * data->line_len + x * (data->bpp / 8));
-	*(unsigned int *)(data->addr + offset) = color;
-}
-
-t_complex	sum_complex(t_complex z1, t_complex z2)
-{
-	t_complex	result;
-
-	result.r = z1.r + z2.r;
-	result.i = z1.i + z2.i;
-	return (result);
-}
-
-t_complex	distributive(t_complex z)
-{
-	t_complex	result;
-
-	result.r = (z.r * z.r) - (z.i * z.i);
-	result.i = 2 * z.r * z.i;
-	return (result);
+	return (ITERATIONS);
 }
