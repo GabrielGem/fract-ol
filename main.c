f1dab309a911907164c01321bf8284092b43bb48
GabrielGem <gabrielgarciamendes91@gmail.com>
2025-11-27 15:08:25 -0300

    adding events

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 17:41:38 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/11/26 14:37:12 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2025/11/27 11:06:32 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +34,15 @@ void	fractol_init(t_fractol *fractol)
 	fractol->mlx_win = mlx_new_window(
 			fractol->mlx, WIDTH, HEIGHT, fractol->name_win);
 	fractol->img.layer = mlx_new_image(fractol->mlx, WIDTH, HEIGHT);
-	fractol->img.addr = mlx_get_data_addr(
-			fractol->img.layer,
-			&fractol->img.bpp,
-			&fractol->img.line_len,
-			&fractol->img.endian);
-	fractol->coordinates.r = (2.0 - (-2.0)) / WIDTH;
-	fractol->coordinates.i = (-2.0 - 2.0) / HEIGHT;
+	fractol->img.addr = mlx_get_data_addr(fractol->img.layer, &fractol->img.bpp,
+			&fractol->img.line_len, &fractol->img.endian);
+	fractol->range = (t_range){.min = (t_complex){.r = -2.0, .i = -2.0},
+			.max = (t_complex){.r = 2.0, .i = 2.0}};
+	fractol->coordinates.r = (fractol->range.max.r - (fractol->range.min.r)) / WIDTH;
+	fractol->coordinates.i = (fractol->range.min.i - fractol->range.max.i) / HEIGHT;
+	fractol->axis = (t_axes){.x = 0.0, .y = 0.0};
+	fractol->zoom = 1.0;
+	event_manager(fractol);
 }
 
 void	fractol_render(t_fractol *fractol)
@@ -57,7 +59,7 @@ void	fractol_render(t_fractol *fractol)
 		x = 0;
 		while (x < WIDTH)
 		{
-			out = handle_pixel(x, y, fractol->coordinates);
+			out = handle_pixel(x, y, fractol);
 			if (out == ITERATIONS)
 				*pixel_ptr = BLACK;
 			else
@@ -71,15 +73,15 @@ void	fractol_render(t_fractol *fractol)
 		fractol->img.layer, 0, 0);
 }
 
-int	handle_pixel(int x, int y, t_complex coordinates)
+int	handle_pixel(int x, int y, t_fractol *fractol)
 {
 	t_complex	z;
 	t_complex	c;
 	int			i;
 
 	z = (t_complex){.r = 0.0, .i = 0.0};
-	c.r = -2.0 + (x * coordinates.r);
-	c.i = 2.0 + (y * coordinates.i);
+	c.r = (fractol->range.min.r + (x * fractol->coordinates.r) * fractol->zoom) + fractol->axis.x;
+	c.i = (fractol->range.max.i + (y * fractol->coordinates.i) * fractol->zoom) + fractol->axis.y;
 	i = 0;
 	while (i < ITERATIONS)
 	{
