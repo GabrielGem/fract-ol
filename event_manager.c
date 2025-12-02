d66f71a1182cdba8dc020d6ac41e715d78625316
gabrgarc <gabrgarc@student.42sp.org.br>
2025-12-02 09:47:54 -0300

    semi refactored due to norminette

--- event_manager.c
+++ event_manager.c
@@ -6,11 +6,16 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 16:15:30 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/11/29 17:32:42 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2025/12/02 09:34:50 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+int		key_map(int keysym, t_fractol *fractol);
+void	arrow_keys(int keysym, t_range *range, t_axes *shift);
+int		close_program(t_fractol *mlx);
+int		mouse_scroll(int button, int x, int y, t_fractol *fractol);
 
 void	event_manager(t_fractol *mlx)
 {
@@ -23,32 +28,39 @@ int	key_map(int keysym, t_fractol *fractol)
 {
 	t_axes	shift;
 
-	shift.x = (fractol->range.max.r - fractol->range.min.r) * 0.05;
-	shift.y = (fractol->range.max.i - fractol->range.min.i) * 0.05;
 	if (keysym == XK_Escape)
 		close_program(fractol);
+	shift.x = (fractol->range.max.r - fractol->range.min.r) * 0.05;
+	shift.y = (fractol->range.max.i - fractol->range.min.i) * 0.05;
+	if (keysym == XK_Left || keysym == XK_Right
+		|| keysym == XK_Up || keysym == XK_Down)
+		arrow_keys(keysym, &fractol->range, &shift);
+	fractol_render(fractol);
+	return (0);
+}
+
+void	arrow_keys(int keysym, t_range *range, t_axes *shift)
+{
 	if (keysym == XK_Left)
 	{
-		fractol->range.min.r -= shift.x;
-		fractol->range.max.r -= shift.x;
+		range->min.r -= shift->x;
+		range->max.r -= shift->x;
 	}
 	if (keysym == XK_Right)
 	{
-		fractol->range.min.r += shift.x;
-		fractol->range.max.r += shift.x;
+		range->min.r += shift->x;
+		range->max.r += shift->x;
 	}
 	if (keysym == XK_Up)
 	{
-		fractol->range.min.i += shift.y;
-		fractol->range.max.i += shift.y;
+		range->min.i += shift->y;
+		range->max.i += shift->y;
 	}
 	if (keysym == XK_Down)
 	{
-		fractol->range.min.i -= shift.y;
-		fractol->range.max.i -= shift.y;
+		range->min.i -= shift->y;
+		range->max.i -= shift->y;
 	}
-	fractol_render(fractol);
-	return (0);
 }
 
 int	close_program(t_fractol *mlx)
@@ -60,20 +72,20 @@ int	close_program(t_fractol *mlx)
 	exit(1);
 }
 
-int	mouse_scroll(int button, int x, int y, t_fractol *fractol)
+int	mouse_scroll(int button, int x, int y, t_fractol *plan)
 {
-	t_complex	mouse_position;
+	t_complex	cursor;
 
 	if (button == 4)
-		fractol->zoom = 0.99;
+		plan->zoom = 0.99;
 	if (button == 5)
-		fractol->zoom = 1.01;
-	mouse_position.r = fractol->range.min.r + (x * fractol->coordinates.r);
-	mouse_position.i = fractol->range.max.i + (y * fractol->coordinates.i);
-	fractol->range.min.r = mouse_position.r + ((fractol->range.min.r - mouse_position.r) * fractol->zoom);
-	fractol->range.max.r = mouse_position.r + ((fractol->range.max.r - mouse_position.r) * fractol->zoom);
-	fractol->range.min.i = mouse_position.i + ((fractol->range.min.i - mouse_position.i) * fractol->zoom);
-	fractol->range.max.i = mouse_position.i + ((fractol->range.max.i - mouse_position.i) * fractol->zoom);
-	fractol_render(fractol);
+		plan->zoom = 1.01;
+	cursor.r = plan->range.min.r + (x * plan->coordinates.r);
+	cursor.i = plan->range.max.i + (y * plan->coordinates.i);
+	plan->range.min.r = cursor.r + (plan->range.min.r - cursor.r) * plan->zoom;
+	plan->range.max.r = cursor.r + (plan->range.max.r - cursor.r) * plan->zoom;
+	plan->range.min.i = cursor.i + (plan->range.min.i - cursor.i) * plan->zoom;
+	plan->range.max.i = cursor.i + (plan->range.max.i - cursor.i) * plan->zoom;
+	fractol_render(plan);
 	return (0);
 }
